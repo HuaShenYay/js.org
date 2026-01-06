5d81828128969aa32cc26608c326c23c0e52db4b
HuaShenYay <63533663+HuaShenYay@users.noreply.github.com>
2026-01-06 23:10:12 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2079,6 +2079,7 @@ var cnames_active = {
   "mosaic": "tilework.github.io/mosaic.js.org",
   "mostage": "mirmousaviii.github.io/mostage",
   "motion-live": "cname.vercel-dns.com", // noCF
+  "monji":"https://portfolio.huashenyay.deno.net/"
   "mp": "cncdn.github.io",
   "mp3tag": "eidoriantan.github.io/mp3tag.js",
   "mpe": "weareroli.github.io/mpejs", // noCF? (don´t add this in a new PR)
